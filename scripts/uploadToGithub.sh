929dd1b54fa6490afc82aafbbe084bed9e713ddc
Hannah Sofie <Hannah-Sofie@live.no>
2022-12-04 14:22:45 -0400

    Changes at Sun 04 Dec 2022 02:22:45 PM -04

--- scripts/uploadToGithub.sh
+++ scripts/uploadToGithub.sh
@@ -4,13 +4,13 @@ if ! [ -d "/opt/weather/.git" ]; then
 
     git init
 
-    git config --global user.name "Hannah Sofie"
-    git config --global user.email "Hannah-Sofie@live.no"
-    git config --global user.password "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"
+    # git config --global user.name "Hannah Sofie"
+    # git config --global user.email "Hannah-Sofie@live.no"
+    
     # H@nnah2022
     git config credential.helper store
 
-    git remote add origin "https://github.com/Hannah-Sofie/Weather.git"
+    git remote add origin "https://github.com/Hannah-Sofie/weather.git"
 fi
 
 
