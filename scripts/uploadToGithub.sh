3ef80ae714ffa1eac88cbd58e3704837729ba283
Hannah <90150033+Hannah-Sofie@users.noreply.github.com>
2022-12-07 21:26:34 +0100

    Update uploadToGithub.sh
    Adding some comments

--- scripts/uploadToGithub.sh
+++ scripts/uploadToGithub.sh
@@ -9,8 +9,23 @@ if ! [ -d "/opt/weather/.git" ]; then
 
     # git config --global user.name "Hannah Sofie"
     # git config --global user.email "Hannah-Sofie@live.no"
+    # sudo git config --global user.password "<token>"
+    # access token = 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
     
-    # H@nnah2022
+    # github token works only one time
+    # script needs push the code automatically
+    # have to store the user and password to avoid write
+    # them when git command ask for the credentials, so I use
+    # git config user.name ""
+    # git config user.email ""
+    # git config user.password ""
+    
+    # didn't want to change the global values
+    # but if I don't add "--global" on the commands this data
+    # is saved on .git directory and when "push" is done
+    # these values are also pushed. And if github detect
+    # on the repositories, automatically are revoked.
+    
     sudo git config credential.helper store
 
     sudo git remote add origin "https://github.com/Hannah-Sofie/weather.git"
